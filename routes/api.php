f7c40d3c4928ae86b4ef06455b6ef06dc42159d5
refan8881 <refan7151@gmail.com>
2021-02-04 22:00:24 +1200

    api prov-desa-kota-indonesia

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+
 use App\Models\Provinsi;
+use App\Models\Kasus;
+use App\Models\Kota;
+use App\Models\Kecamatan;
+use App\Models\Kelurahan;
+use App\Models\Desa;
+use App\Models\Rw;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProvinsiController;
@@ -19,6 +26,14 @@ use App\Http\Controllers\Api\ProvinsiController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/hari', [ProvinsiController::class,'hari']);
+Route::get('/indo', [ProvinsiController::class,'indonesia']);
+Route::get('/provinsi1/{id}', [ProvinsiController::class,'provinsi']);
+Route::get('/provinsi2', [ProvinsiController::class,'provinsi1']);
+Route::get('/kota', [ProvinsiController::class,'kota']);
+Route::get('/kecamatan', [ProvinsiController::class,'kecamatan']);
+Route::get('/desa', [ProvinsiController::class,'desa']);
 
 // API Provinsi
 Route::get('provinsi', [ProvinsiController::class, 'index']);
